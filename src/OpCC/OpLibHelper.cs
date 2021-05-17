171b54e31382d8f39bce620ff75cf6d0ca4c173f
OtmarP (opMedion14b) <Otmar.Pilgerstorfer@chello.at>
2021-05-17 13:20:36 +0200

    mit Parameter -display:Text -c:fb, v:1.0.18.321 #1, #3

---
+++ src/OpCC/OpLibHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace OpLib
+{
+    public static class Helper
+    {
+        public static string AddDEVToVersion(string versionNumber, bool launchedFromStudio)
+        {
+            if (launchedFromStudio)
+            {
+                versionNumber = string.Concat(versionNumber, ".[DEV]");
+            }
+            return versionNumber;
+        }
+
+        public static string AddRDToVersion(string versionNumber)
+        {
+#if DEBUG
+            versionNumber = string.Concat(versionNumber, ".D");
+#else
+            versionNumber = string.Concat(versionNumber, ".R");
+#endif
+            return versionNumber;
+        }
+
+        //        public static void AddRDToVersion()
+        //        {
+        //#if DEBUG
+        //            VersionNumber += ".D";
+        //#else
+        //            VersionNumber+=".R";
+        //#endif
+        //        }
+
+        public static string AddSystemInfo(string systemInfo)
+        {
+            if (!Environment.Is64BitProcess)
+            {
+                systemInfo = "32Bit";
+            }
+            else
+            {
+                systemInfo = "64Bit";
+            }
+            systemInfo = string.Concat(systemInfo, ", ", Environment.Version.ToString());
+            systemInfo = string.Concat(systemInfo, ", OS:");
+            if (!Environment.Is64BitOperatingSystem)
+            {
+                systemInfo = string.Concat(systemInfo, "32Bit");
+            }
+            else
+            {
+                systemInfo = string.Concat(systemInfo, "64Bit");
+            }
+            systemInfo = string.Concat(systemInfo, ", ", Environment.OSVersion.ToString());
+            return systemInfo;
+        }
+
+        public static bool CheckLaunchedFromStudio()
+        {
+            Process.GetCurrentProcess().MainModule.ModuleName.Contains(".vshost");
+            return Debugger.IsAttached;
+        }
+
+        public static string FormatDate(DateTime date, string format)
+        {
+            return date.ToString(format).Replace("..", ".");
+        }
+    }
+}
--- src/OpCC/Program.cs
+++ src/OpCC/Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using OpLib;
+using System;
 using System.Threading;
 
 namespace OpCC
@@ -13,109 +11,104 @@ namespace OpCC
         // EnvironmentVariableTarget.User ...... HKEY_CURRENT_USER\Environment 
 
         public const string _OPCC_TM_START = "OPCC_TM_START";
+
         public static bool _launchedFromStudio;
-        public static string VersionNumber = "";
 
         static void Main(string[] args)
         {
-            VersionNumber = "1.0.15.1202";
-            bool flag = false;
-            //bool flag = Helper.CheckLaunchedFromStudio();
+            string version = "1.0.18.321";    // "1.0.15.1202";
+            bool flag = Helper.CheckLaunchedFromStudio();
             _launchedFromStudio = flag;
-            AddRDToVersion();
-            //version = Helper.AddRDToVersion(version);
+            version = Helper.AddRDToVersion(version);
 
             // Check Parameter
             if (args.Length == 0)
             {
-                DisplayNoParameter();
+                DisplayNoParameter(version);
                 return;
             }
-            else
+            if (args[0] == "-?")
             {
-                if (args[0] == "-?")
-                {
-                    DisplayHelp();
-                    return;
-                }
-
-                // -clear
-                if (args[0] == "-clear")
-                {
-                    ClearEnvironmentVariables();
-                    return;
-                }
-
-                // -start, -time, -stop
-                if (args[0] == "-start")
-                {
-                    TMStart();
-                    return;
-                }
-                else if (args[0] == "-time")
-                {
-                    TMStop(false);
-                    return;
-                }
-                else if (args[0] == "-stop")
-                {
-                    TMStop(true);
-                    return;
-                }
-                else if (args[0].StartsWith("-wait:"))
-                {
-                    // OpCC -wait:5
-                    WaitSeconds(args[0]);
-                    return;
-                }
-
-                // next...
-                // ToDo: -display:Text -c:0C
-
-                DisplayWrongParameter();
+                DisplayHelp(version);
+                return;
             }
+
+            // -clear
+            if (args[0] == "-clear")
+            {
+                ClearEnvironmentVariables();
+                return;
+            }
+
+            // -start, -time, -stop
+            if (args[0] == "-start")
+            {
+                TMStart();
+                return;
+            }
+            if (args[0] == "-time")
+            {
+                TMStop(false);
+                return;
+            }
+            if (args[0] == "-stop")
+            {
+                TMStop(true);
+                return;
+            }
+
+            // OpCC -wait:5
+            if (args[0].StartsWith("-wait:"))
+            {
+                WaitSeconds(args[0]);
+                return;
+            }
+            if (!args[0].StartsWith("-display:"))
+            {
+                WaitInIDE(flag);
+                DisplayWrongParameter(version);
+                return;
+            }
+            string str = "";
+            if ((int)args.Length == 2)
+            {
+                str = args[1];
+            }
+            DisplayText(args[0], str);
         }
 
-        public static void AddRDToVersion()
-        {
-#if DEBUG
-            VersionNumber += ".D";
-#else
-            VersionNumber+=".R";
-#endif
-        }
-
-        public static void DisplayNoParameter()
+        public static void DisplayNoParameter(string versionNumber)
         {
             // Display Help
-            DisplayHeaderAndHelp(true, "Warning: no Parameter.", true);
-
-            System.Environment.Exit(0);
-            return;
+            Console.ForegroundColor = ConsoleColor.Red;
+            DisplayHeaderAndHelp(true, versionNumber, "Warning: no Parameter.", true);
+            Console.ResetColor();
+            WaitInIDE(_launchedFromStudio);
+            Environment.Exit(0);
         }
 
-        public static void DisplayHelp()
+        public static void DisplayHelp(string versionNumber)
         {
             // Display Help
-            DisplayHeaderAndHelp(true, "", true);
+            DisplayHeaderAndHelp(true, versionNumber, "", true);
 
-            System.Environment.Exit(0);
-            return;
+            Environment.Exit(0);
         }
 
-        public static void DisplayWrongParameter()
+        public static void DisplayWrongParameter(string versionNumber)
         {
-            DisplayHeaderAndHelp(true, "Warning: wrong Parameter.", false);
+            Console.ForegroundColor = ConsoleColor.Red;
+            DisplayHeaderAndHelp(true, versionNumber, "Warning: wrong Parameter.", false);
+            Console.ResetColor();
 
-            System.Environment.Exit(1);
-            return;
+            Environment.Exit(1);
         }
 
-        public static void DisplayHeaderAndHelp(bool dispHeader, string message, bool dispHelp)
+        public static void DisplayHeaderAndHelp(bool dispHeader, string versionNumber, string message, bool dispHelp)
         {
             if (dispHeader)
             {
-                Console.WriteLine("OpCC v:" + VersionNumber);
+                Console.WriteLine(string.Concat("OpCC v:", versionNumber));
             }
             Console.WriteLine(message);
             if (dispHelp)
@@ -124,6 +117,17 @@ namespace OpCC
                 Console.WriteLine("OpCC -start|-time|-stop .... TM - TimeMark, NCC - Norton Control Center");
                 Console.WriteLine("OpCC -clear ................ Clear all EnvironmentVariables");
                 Console.WriteLine("OpCC -wait:5 ............... Waits for 5 Seconds");
+                Console.WriteLine("OpCC -display:Text -c:0C ... Display 'Text' in Color Red");
+                Console.WriteLine("OpCC -display:Text -c:0A ... Display 'Text' in Color Green");
+                Console.WriteLine("                               -c:BF the first corresponds to the background, the second the foreground");
+                Console.WriteLine("                               0 = Black       8 = Gray");
+                Console.WriteLine("                               1 = Blue        9 = Light Blue");
+                Console.WriteLine("                               2 = Green       A = Light Green");
+                Console.WriteLine("                               3 = Aqua        B = Light Aqua");
+                Console.WriteLine("                               4 = Red         C = Light Red");
+                Console.WriteLine("                               5 = Purple      D = Light Purple");
+                Console.WriteLine("                               6 = Yellow      E = Light Yellow");
+                Console.WriteLine("                               7 = White       F = Bright White");
             }
         }
 
@@ -270,8 +274,134 @@ namespace OpCC
                 Console.Write(".");
             }
             Console.WriteLine("OK");
-            Program.WaitInIDE(_launchedFromStudio);
+            WaitInIDE(_launchedFromStudio);
             Environment.Exit(0);
+        }
+
+        public static void DisplayText(string para, string color)
+        {
+            string str = "";
+            string upper = "";
+            string[] strArrays = para.Split(new char[] { ':' });
+            if ((int)strArrays.Length >= 2)
+            {
+                for (int i = 1; i < (int)strArrays.Length; i++)
+                {
+                    if (i >= 2)
+                    {
+                        str = string.Concat(str, ":");
+                    }
+                    str = string.Concat(str, strArrays[i]);
+                }
+            }
+            str = str.Replace("\\\\r", "\r");
+            str = str.Replace("\\\\n", "\n");
+            str = str.Replace("\\\\r\\n", "\r\n");
+            str = str.Replace("\\\\t", "\t");
+            string[] strArrays1 = color.Split(new char[] { ':' });
+            if ((int)strArrays1.Length >= 2)
+            {
+                upper = strArrays1[1];
+            }
+            upper = upper.ToUpper();
+            int? coloFromHexDigit = null;
+            int? nullable = null;
+            if (upper != "")
+            {
+                if (upper.Length == 1)
+                {
+                    nullable = GetColoFromHexDigit(upper);
+                }
+                else if (upper.Length == 2)
+                {
+                    string str1 = upper.Substring(0, 1);
+                    string str2 = upper.Substring(1, 1);
+                    coloFromHexDigit = GetColoFromHexDigit(str1);
+                    nullable = GetColoFromHexDigit(str2);
+                }
+            }
+            if (coloFromHexDigit.HasValue)
+            {
+                Console.BackgroundColor = (ConsoleColor)coloFromHexDigit.Value;
+            }
+            if (nullable.HasValue)
+            {
+                Console.ForegroundColor = (ConsoleColor)nullable.Value;
+            }
+            Console.WriteLine(str);
+            Console.ResetColor();
+            WaitInIDE(_launchedFromStudio);
+            Environment.Exit(0);
+        }
+
+        public static int? GetColoFromHexDigit(string dispColorDigit)
+        {
+            int? nullable = null;
+            if (dispColorDigit == "0")
+            {
+                nullable = new int?(0);
+            }
+            if (dispColorDigit == "1")
+            {
+                nullable = new int?(1);
+            }
+            if (dispColorDigit == "2")
+            {
+                nullable = new int?(2);
+            }
+            if (dispColorDigit == "3")
+            {
+                nullable = new int?(3);
+            }
+            if (dispColorDigit == "4")
+            {
+                nullable = new int?(4);
+            }
+            if (dispColorDigit == "5")
+            {
+                nullable = new int?(5);
+            }
+            if (dispColorDigit == "6")
+            {
+                nullable = new int?(6);
+            }
+            if (dispColorDigit == "7")
+            {
+                nullable = new int?(7);
+            }
+            if (dispColorDigit == "8")
+            {
+                nullable = new int?(8);
+            }
+            if (dispColorDigit == "9")
+            {
+                nullable = new int?(9);
+            }
+            if (dispColorDigit == "A")
+            {
+                nullable = new int?(10);
+            }
+            if (dispColorDigit == "B")
+            {
+                nullable = new int?(11);
+            }
+            if (dispColorDigit == "C")
+            {
+                nullable = new int?(12);
+            }
+            if (dispColorDigit == "D")
+            {
+                nullable = new int?(13);
+            }
+            if (dispColorDigit == "E")
+            {
+                nullable = new int?(14);
+            }
+            if (dispColorDigit == "F")
+            {
+                nullable = new int?(15);
+            }
+            return nullable;
         }
     }
 }
